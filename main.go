0f8d06b502be3a8d498e7c08c6b5745d213d2388
Pedro Gomes <pedromgomes29@gmail.com>
2024-08-16 14:10:03 +0100

    refactoring: blockchain node initial REST interface

--- main.go
+++ main.go
@@ -1,72 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"flag"
 
 	"github.com/pedrogomes29/blockchain/server"
-	"github.com/pedrogomes29/blockchain/wallet"
 )
 
 func main() {
-	// Create two wallets
-	minerWallet := wallet.NewWalletAndPrivateKey()
+	minerAddr := flag.String("miner", "", "Miner's wallet address")
+	flag.Parse()
 
-	wallet1 := wallet.NewWalletAndPrivateKey()
-
-	wallet2 := wallet.NewWalletAndPrivateKey()
-
-	// Start a server with the first wallet's address
-	server := server.NewServer(minerWallet.Address())
-	go server.Run()
-
-	minerWallet.SetServer(server)
-	wallet1.SetServer(server)
-	wallet2.SetServer(server)
-	fmt.Printf("Initial balance of %s: %d\n", wallet1.Address(), wallet1.GetBalance())
-	fmt.Printf("Initial balance of %s: %d\n", wallet2.Address(), wallet2.GetBalance())
-	fmt.Printf("Miner balance: %d\n", minerWallet.GetBalance())
-
-	time.Sleep(3 * time.Second)
-
-	minerWallet.SendToAddress(wallet1.Address(), 10)
-
-	time.Sleep(3 * time.Second)
-
-	// Initial balances
-	fmt.Printf("Initial balance of %s: %d\n", wallet1.Address(), wallet1.GetBalance())
-	fmt.Printf("Initial balance of %s: %d\n", wallet2.Address(), wallet2.GetBalance())
-	fmt.Printf("Miner balance: %d\n", minerWallet.GetBalance())
-
-	// Perform some transactions
-	fmt.Println("Performing transactions...")
-
-	wallet1.SendToAddress(wallet2.Address(), 10)
-
-	// Send 10 coins from wallet1 to wallet2
-	time.Sleep(3 * time.Second)
-
-	// Check balances after the first transaction
-	fmt.Printf("Balance of %s after sending 10 coins: %d\n", wallet1.Address(), wallet1.GetBalance())
-	fmt.Printf("Balance of %s after receiving 10 coins: %d\n", wallet2.Address(), wallet2.GetBalance())
-	fmt.Printf("Miner balance: %d\n", minerWallet.GetBalance())
-
-	// Send 5 coins back from wallet2 to wallet1
-	wallet2.SendToAddress(wallet1.Address(), 5)
-	time.Sleep(3 * time.Second)
-
-	// Check balances after the second transaction
-	fmt.Printf("Balance of %s after receiving 5 coins: %d\n", wallet1.Address(), wallet1.GetBalance())
-	fmt.Printf("Balance of %s after sending 5 coins: %d\n", wallet2.Address(), wallet2.GetBalance())
-	fmt.Printf("Miner balance: %d\n", minerWallet.GetBalance())
-
-	// Send 2 coins from wallet1 to wallet2
-	wallet1.SendToAddress(wallet2.Address(), 2)
-	time.Sleep(3 * time.Second)
-
-	// Final balances
-	fmt.Printf("Balance of %s after sending 2 coins: %d\n", wallet1.Address(), wallet1.GetBalance())
-	fmt.Printf("Balance of %s after receiving 3 coins: %d\n", wallet2.Address(), wallet2.GetBalance())
-	fmt.Printf("Miner balance: %d\n", minerWallet.GetBalance())
-
+	server := server.NewServer(*minerAddr)
+	server.Run()
 }
--- server/server.go
+++ server/server.go
@@ -1,58 +1,94 @@
 package server
 
 import (
-	"fmt"
+	"net/http"
 	"sync"
 
+	"github.com/gin-gonic/gin"
 	"github.com/pedrogomes29/blockchain/blockchain"
 	"github.com/pedrogomes29/blockchain/transactions"
 )
 
-
-type Server struct{
-	Bc *blockchain.Blockchain
-	minerAddress string
+type Server struct {
+	bc              *blockchain.Blockchain
+	minerAddress    string
 	blockInProgress *blockchain.Block
-	mu               sync.Mutex
+	mu              sync.Mutex
 }
 
-func NewServer(minerAddress string) *Server{
+func NewServer(minerAddress string) *Server {
 	return &Server{
-		Bc: blockchain.NewBlockchain(minerAddress),
+		bc:           blockchain.NewBlockchain(minerAddress),
 		minerAddress: minerAddress,
 	}
 }
 
-func (server *Server) AddTransaction(tx *transactions.Transaction){
-	if(!tx.VerifyInputSignatures(server.Bc.ChainstateDB)){
-		fmt.Println("Transaction inputs have at least one invalid signature")
-		return;
+func (server *Server) AddTransactionHandler(c *gin.Context) {
+	var tx transactions.Transaction
+
+	if err := c.ShouldBindJSON(&tx); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid transaction format"})
+		return
+	}
+
+	if !tx.VerifyInputSignatures(server.bc.ChainstateDB) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Transaction inputs have at least one invalid signature"})
+		return
 	}
 
 	server.mu.Lock()
 	defer server.mu.Unlock()
 
-	if server.blockInProgress != nil{
-		server.blockInProgress.Transactions = append(server.blockInProgress.Transactions, tx)
+	if server.blockInProgress != nil {
+		server.blockInProgress.Transactions = append(server.blockInProgress.Transactions, &tx)
 	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "Transaction added to mempool successfully"})
 }
 
+func (server *Server) FindUTXOsHandler(c *gin.Context) {
+	var pubKeyHash []byte
 
-func (server *Server) Run(){
+	if err := c.ShouldBindJSON(&pubKeyHash); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid public key hash format"})
+		return
+	}
+
+	utxos, err := server.bc.FindUTXOs(pubKeyHash)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error finding UTXOs"})
+		return
+	}
+
+	c.JSON(http.StatusOK, utxos)
+}
+
+func (server *Server) Run() {
 	done := make(chan struct{})
-	for {
-		server.mu.Lock()
-		if server.blockInProgress!=nil {
-			server.Bc.AddBlock(server.blockInProgress)
 
+	go func() {
+		for {
+			server.mu.Lock()
+			if server.blockInProgress != nil {
+				server.bc.AddBlock(server.blockInProgress)
+			}
+			server.blockInProgress = blockchain.NewBlock(
+				[]*transactions.Transaction{transactions.NewCoinbaseTX(server.minerAddress)},
+				server.bc.LastBlockHash,
+			)
+			server.mu.Unlock()
+
+			go server.blockInProgress.POW(done)
+			<-done
 		}
-		server.blockInProgress = blockchain.NewBlock(
-			[]*transactions.Transaction{transactions.NewCoinbaseTX(server.minerAddress)},
-			server.Bc.LastBlockHash,
-		)
-		server.mu.Unlock()
+	}()
 
-		go server.blockInProgress.POW(done)
-		<-done
+	r := gin.Default()
+	r.POST("/transaction", server.AddTransactionHandler)
+	r.GET("/utxos", server.FindUTXOsHandler)
+
+	// Start the HTTP server
+	if err := r.Run(":8080"); err != nil {
+		panic("Failed to run server: " + err.Error())
 	}
 }
